8ab60c31c13ead38648730be6547c5ed5a2a8ea9
Aubrey Howell <simplyaubs@gmail.com>
2014-07-18 11:53:28 -0600

    Fix Users table migration.

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140718035107) do
+ActiveRecord::Schema.define(version: 20140718173511) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: true do |t|
-    t.string   "provider"
-    t.string   "uid"
-    t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "provider"
+    t.string "uid"
   end
 
 end
