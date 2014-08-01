ffe4ec0ddc85c4ea284b33b17bd2aaf85d5a23b9
Aubrey Howell <simplyaubs@gmail.com>
2014-07-31 21:11:30 -0600

    fixed feature test typo

--- spec/features/book_appt_spec.rb
+++ spec/features/book_appt_spec.rb
@@ -8,7 +8,7 @@ feature 'Mentee Appointments' do
     visit root_path
     find('#login a').click
     mock_omniauth
-    click_on 'Book Mentoring Session'
+    click_on 'Book a Mentoring Session'
 
     click_on 'Book Session'
 
