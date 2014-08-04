d91edfed63c9bbb94ba1fdd2fcf57a490932f3bb
Aubrey Howell <simplyaubs@gmail.com>
2014-08-04 16:03:19 -0600

    validate time column not nil

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,8 +3,6 @@ class Appointment < ActiveRecord::Base
   belongs_to :mentor, class_name: 'User'
   belongs_to :mentee, class_name: 'User'
 
+  validates :time, presence: true
 
-  # def users
-  #   {mentor: mentor, mentee: mentee}
-  # end
 end
